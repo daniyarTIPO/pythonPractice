de6cd2ad8ccfdd7c1a1dd74764f0fde70d7bfba3
Daniyar <pssghoil@gmail.com>
2024-05-20 14:31:03 +0600

    Dice Simulator

--- (6)Dice Simulator/main.py
+++ (6)Dice Simulator/main.py
@@ -1,4 +1,18 @@
+import random
 
+def roll_dice():
+    return random.randint(1, 6)
+
+def dice_game():
+    print("Кости!")
+    play_again = True
+
+    while play_again:
+        input("Нажмите ENTER,чтобы бросить кости...")
+        dice1 = roll_dice()
+        dice2 = roll_dice()
+        print(f"Вы бросили кости и выпало: {dice1} and {dice2}")
+        print(f"Общая сумма: {dice1 + dice2}")
 
 if __name__ == "__main__":
-    pass
+    dice_game()
